ead0c7c8e939b504373ffe429682866cd17ff2af
Chowdhury Md Rajib Sarwar <md.rajib.sarwar@gmail.com>
2022-07-29 21:04:28 -0400

    Update Predictions.py

--- Predictions.py
+++ Predictions.py
@@ -1,90 +1,70 @@
-#import findspark
-#findspark.init()
-#findspark.find()
+import quinn
+import requests
 
-#Loading the libraries
-import pyspark
-from pyspark.mllib.tree import RandomForest, RandomForestModel
-from pyspark.mllib.util import MLUtils
-from pyspark import SparkContext, SparkConf
-from pyspark.sql import SparkSession
-from pyspark.ml.feature import VectorAssembler
-from pyspark.mllib.regression import LabeledPoint
-from pyspark.sql.functions import col
-from pyspark.mllib.linalg import Vectors
-from pyspark import SparkContext, SparkConf
-from pyspark.sql.session import SparkSession	
-from pyspark.mllib.evaluation import MulticlassMetrics
+from pyspark.ml.feature import VectorAssembler, Normalizer, StandardScaler
+from pyspark.ml.classification import LogisticRegression
+
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 from pyspark.ml import Pipeline
-import sys
-import pandas as pd
-import numpy as np
-from sklearn.metrics import f1_score
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
 
-#Starting the spark session
-conf = pyspark.SparkConf().setAppName('winequality').setMaster('local')
-sc = pyspark.SparkContext(conf=conf)
+sc = SparkContext('local')
 spark = SparkSession(sc)
 
-path  = sys.argv[1]
-#loading the validation dataset
-val = spark.read.format("csv").load(path, header = True , sep=";")
-val.printSchema()
-val.show()
+train_df = spark.read.format('csv').options(header='true', inferSchema='true', sep=';').load('TrainingDataset.csv')
+test_df = spark.read.format('csv').options(header='true', inferSchema='true', sep=';').load('ValidationDataset.csv')
+#Undo this line to test the dataset with your datasettest_df = spark.read.format('csv').options(header='true', inferSchema='true', sep=';').load('ValidationDataset.csv')
+#test_df = spark.read.format('csv').options(header='true', inferSchema='true', sep=';').load('TestDataset.csv')
 
-    
-#changing the 'quality' column name to 'label'
-for col_name in val.columns[1:-1]+['""""quality"""""']:
-    val = val.withColumn(col_name, col(col_name).cast('float'))
-val = val.withColumnRenamed('""""quality"""""', "label")
+print("Data loaded into Spark.")
+print(train_df.toPandas().head())
 
-#getting the features and label seperately and converting it to numpy array
-features =np.array(val.select(val.columns[1:-1]).collect())
-label = np.array(val.select('label').collect())
+def remove_quotations(s):
+    return s.replace('"', '')
 
-#creating the feature vector
-VectorAssembler = VectorAssembler(inputCols = val.columns[1:-1] , outputCol = 'features')
-df_tr = VectorAssembler.transform(val)
-df_tr = df_tr.select(['features','label'])
+train_df = quinn.with_columns_renamed(remove_quotations)(train_df)
+train_df = train_df.withColumnRenamed('quality', 'label')
 
-#The following function creates the labeledpoint and parallelize it to convert it into RDD
-def to_labeled_point(sc, features, labels, categorical=False):
-    labeled_points = []
-    for x, y in zip(features, labels):        
-        lp = LabeledPoint(y, x)
-        labeled_points.append(lp)
-    return sc.parallelize(labeled_points) 
+test_df = quinn.with_columns_renamed(remove_quotations)(test_df)
+test_df = test_df.withColumnRenamed('quality', 'label')
 
-#rdd converted dataset
-dataset = to_labeled_point(sc, features, label)
+print("Data has been formatted.")
+print(train_df.toPandas().head())
 
-#loading the model from s3
-RFModel = RandomForestModel.load(sc, "/winepredict/trainingmodel.model/")
+assembler = VectorAssembler(
+    inputCols=["fixed acidity",
+               "volatile acidity",
+               "citric acid",
+               "residual sugar",
+               "chlorides",
+               "free sulfur dioxide",
+               "total sulfur dioxide",
+               "density",
+               "pH",
+               "sulphates",
+               "alcohol"],
+                outputCol="inputFeatures")
 
-print("model loaded successfully")
-predictions = RFModel.predict(dataset.map(lambda x: x.features))
+scaler = Normalizer(inputCol="inputFeatures", outputCol="features")
 
-#getting a RDD of label and predictions
-labelsAndPredictions = dataset.map(lambda lp: lp.label).zip(predictions)
- 
-labelsAndPredictions_df = labelsAndPredictions.toDF()
-#cpnverting rdd ==> spark dataframe ==> pandas dataframe 
-labelpred = labelsAndPredictions.toDF(["label", "Prediction"])
-labelpred.show()
-labelpred_df = labelpred.toPandas()
+## Only use LogisticRegression because this was the best model decided on
+lr = LogisticRegression()
 
+pipeline1 = Pipeline(stages=[assembler, scaler, lr])
 
-#Calculating the F1score
-F1score = f1_score(labelpred_df['label'], labelpred_df['Prediction'], average='micro')
-print("F1- score: ", F1score)
-print(confusion_matrix(labelpred_df['label'],labelpred_df['Prediction']))
-print(classification_report(labelpred_df['label'],labelpred_df['Prediction']))
-print("Accuracy" , accuracy_score(labelpred_df['label'], labelpred_df['Prediction']))
+paramgrid = ParamGridBuilder().build()
 
-#calculating the test error
-testErr = labelsAndPredictions.filter(
-    lambda lp: lp[0] != lp[1]).count() / float(dataset.count())    
-print('Test Error = ' + str(testErr))
+evaluator = MulticlassClassificationEvaluator(metricName="f1")
 
+crossval = CrossValidator(estimator=pipeline1,  
+                         estimatorParamMaps=paramgrid,
+                         evaluator=evaluator, 
+                         numFolds=3
+                        )
+
+cvModel1 = crossval.fit(train_df) 
+print("Docker run Successful")
+print("F1 Score of this Model ", evaluator.evaluate(cvModel1.transform(test_df)))
